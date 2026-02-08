40e90f2fa2eba8d3201c7184383d6654268f0c9d
Madhav <madhav.gullapalli9@gmail.com>
2026-02-08 16:03:00 -0500

    added reverse mapping

--- visualize_data.py
+++ visualize_data.py
@@ -8,11 +8,11 @@ import argparse
 import os
 
 
-def createTSNE(df):
+def createTSNE(df, encoding):
     #extracting the arch index, arch string, cifar10-valid_test_accuracy, pytorch_code_inline, and 'true_onnx_encoding'
-    df = df[['arch_index', 'arch_string', 'cifar10-valid_test_accuracy', 'pytorch_code_inline', 'modernbert_large_true_onnx_encoding_fp16_embedding']]
+    
     # clean up modernbert encoding by removing brackets and splitting by comma
-    vector = df['modernbert_large_true_onnx_encoding_fp16_embedding'].apply(lambda x: x.strip('[]').replace(' ', ''))
+    vector = df[encoding].apply(lambda x: x.strip('[]').replace(' ', ''))
     #run tsne on the 'true_onnx_encoding' column
     X = vector.apply(lambda x: [float(i) for i in x.split(',')])
     X = np.array(X.tolist())
@@ -55,19 +55,38 @@ def cluster(tsne_x, tsne_y, alpha):
     df['cluster'] = clustering.labels_
     return df
     
+def createReversemapping(df, cluster_df):
+    #create a new df that unifies both dataframes
+    merged_df = pd.concat([df.reset_index(drop=True), cluster_df.reset_index(drop=True)], axis=1)
+    #create a dictionarty that maps clusetrs to arch strings
 
+    reverse_mapping = {}
+    for cluster in merged_df['cluster'].unique():
+        cluster_archs = merged_df[merged_df['cluster'] == cluster]['pytorch_code_inline'].tolist()
+        reverse_mapping[int(cluster)] = cluster_archs
+    return reverse_mapping    
 def main():
     parser = argparse.ArgumentParser(description='t-SNE Visualization of Genome Embeddings')
     parser.add_argument('--input', type=str, default=f'/storage/ice-shared/vip-vvk/data/AOT/psomu3/codenas2/nasbench201_corpus_onnx_paper_embedded.csv', help='Path to the input CSV file containing the data')
     parser.add_argument('--output_dir', type=str, default=f'/storage/ice-shared/vip-vvk/data/AOT/mgullapalli6/codenas2/tsne_output', help='Directory to save the output visualization')
     parser.add_argument('--output_prefix', type=str, default='onnx_main', help='Prefix for the output visualization file name')
-
+    parser.add_argument('--encoding', type=str, default='modernbert_large_true_onnx_encoding_fp16_embedding', help='Column name for the encoding to be visualized')
+    parser.add_argument('--test_field', type=str, default='cifar10-valid_test_accuracy', help='Column name for the field to be plotted as hue in the gradient plot')
     args = parser.parse_args()
-
+    print("starting t-SNE visualization with the following arguments:")
+    print(args)
     df = pd.read_csv(args.input)
-    tsne_1, tsne_2,accuracy = createTSNE(df)
+    tsne_1, tsne_2,accuracy = createTSNE(df, args.encoding)
     cluster_df = cluster(tsne_1, tsne_2, alpha=5.0)
-    drawTSNE(tsne_1, tsne_2, args.output_prefix, args.output_dir, df['cifar10-valid_test_accuracy'],"","gradient")
+    print(f"done with clustering, now drawing t-SNE plots")
+    drawTSNE(tsne_1, tsne_2, args.output_prefix, args.output_dir, df[args.test_field],"","gradient")
     drawTSNE(tsne_1, tsne_2, args.output_prefix, args.output_dir, cluster_df['cluster'],"clustered","discrete")
-
+    #save the reverse mapping of cluster to arch index and arch string as a json
+    reverse_mapping = createReversemapping(df, cluster_df)
+    with open(os.path.join(args.output_dir, f'{args.output_prefix}_reverse_mapping.txt'), 'w') as f:
+        for cluster1, archs in reverse_mapping.items():
+            f.write(f"Cluster {cluster1}:\n")
+            for arch in archs:
+                f.write(f"  {arch}\n")
+    print(f"reverse mapping saved at {os.path.join(args.output_dir, f'{args.output_prefix}_reverse_mapping.txt')}")
 main()
