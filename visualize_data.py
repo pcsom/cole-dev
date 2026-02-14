cdf6a2e24a6bc53884e80f995e2159f8e0581993
Madhav <madhav.gullapalli9@gmail.com>
2026-02-14 16:30:34 -0500

    added tsne job and small documentation

--- visualize_data.py
+++ visualize_data.py
@@ -22,7 +22,7 @@ def createTSNE(df, encoding):
 
     #return results
     return X_tsne[:,0], X_tsne[:,1], df['cifar10-valid_test_accuracy']
-def drawTSNE(tsne_1,tsne_2, FILE_NAME, OUTPUT_FOLDER, plotting_number,name, plot_type):
+def drawTSNE(tsne_1,tsne_2, FILE_NAME, OUTPUT_FOLDER, plotting_number,name, plot_type, range = [0,1]):
     OUTPUT_FILE = os.path.join(OUTPUT_FOLDER, FILE_NAME)
     plt.figure(figsize=(10, 8))
     #if plotting type is discreet : use a discret gradient or use continous gradient (deided by plotting number)
@@ -31,7 +31,13 @@ def drawTSNE(tsne_1,tsne_2, FILE_NAME, OUTPUT_FOLDER, plotting_number,name, plot
         list(plt.cm.tab20b.colors) +
         list(plt.cm.tab20c.colors)
     )
-    if(plot_type =="gradient"):
+    #delete data points that are outside the range
+    if(plot_type == "gradient"):
+        mask = (plotting_number >= range[0]) & (plotting_number <= range[1])
+        tsne_1 = tsne_1[mask]
+        tsne_2 = tsne_2[mask]
+        plotting_number = plotting_number[mask]
+
         sns.scatterplot(x=tsne_1, y=tsne_2, hue=plotting_number, palette='viridis')
         plt.legend(title='CIFAR-10 Valid Test Accuracy')
     if(plot_type == "discrete"):
@@ -39,7 +45,7 @@ def drawTSNE(tsne_1,tsne_2, FILE_NAME, OUTPUT_FOLDER, plotting_number,name, plot
         #create an underbar matching colors to clusters
         handles, labels = plt.gca().get_legend_handles_labels()
         plt.legend(handles, labels, title='Cluster', bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.title('t-SNE Visualization of NASBench201 Architectures')
+    plt.title(f'tsne_plot_{name}_{FILE_NAME}')
     plt.xlabel('t-SNE Dimension 1')
     plt.ylabel('t-SNE Dimension 2')
     
@@ -55,14 +61,14 @@ def cluster(tsne_x, tsne_y, alpha):
     df['cluster'] = clustering.labels_
     return df
     
-def createReversemapping(df, cluster_df):
+def createReversemapping(df, cluster_df, code):
     #create a new df that unifies both dataframes
     merged_df = pd.concat([df.reset_index(drop=True), cluster_df.reset_index(drop=True)], axis=1)
     #create a dictionarty that maps clusetrs to arch strings
 
     reverse_mapping = {}
     for cluster in merged_df['cluster'].unique():
-        cluster_archs = merged_df[merged_df['cluster'] == cluster]['pytorch_code_inline'].tolist()
+        cluster_archs = merged_df[merged_df['cluster'] == cluster][code].tolist()
         reverse_mapping[int(cluster)] = cluster_archs
     return reverse_mapping    
 def main():
@@ -70,19 +76,29 @@ def main():
     parser.add_argument('--input', type=str, default=f'/storage/ice-shared/vip-vvk/data/AOT/psomu3/codenas2/nasbench201_corpus_onnx_paper_embedded.csv', help='Path to the input CSV file containing the data')
     parser.add_argument('--output_dir', type=str, default=f'/storage/ice-shared/vip-vvk/data/AOT/mgullapalli6/codenas2/tsne_output', help='Directory to save the output visualization')
     parser.add_argument('--output_prefix', type=str, default='onnx_main', help='Prefix for the output visualization file name')
-    parser.add_argument('--encoding', type=str, default='modernbert_large_true_onnx_encoding_fp16_embedding', help='Column name for the encoding to be visualized')
+    #make it input a list of encodings to be visualized and default to the modernbert_large_true_onnx_encoding_fp16_embedding encoding
+    parser.add_argument('--encodings', type=str, nargs='+', default=['modernbert_large_true_onnx_encoding_fp16_embedding'], help='Column name for the encoding to be visualized')
+    
     parser.add_argument('--test_field', type=str, default='cifar10-valid_test_accuracy', help='Column name for the field to be plotted as hue in the gradient plot')
+    parser.add_argument('--code_field', type=str, default='pytorch_code_inline', help='Column name for the code field to be used in reverse mapping')
+    parser.add_argument('--debug', type=bool, default=True, help='Whether to print debug information, such as the columns of the input dataframe')
+    parser.add_argument('--range', type=int, nargs=2, default=[80,100], help='Range of values to be plotted in the gradient plot')
+    parser.add_argument('--cluster', type=bool, default=True, help='Whether to perform clustering on the t-SNE values and plot the clustered results')
     args = parser.parse_args()
     print("starting t-SNE visualization with the following arguments:")
-    print(args)
     df = pd.read_csv(args.input)
-    tsne_1, tsne_2,accuracy = createTSNE(df, args.encoding)
-    cluster_df = cluster(tsne_1, tsne_2, alpha=5.0)
-    print(f"done with clustering, now drawing t-SNE plots")
-    drawTSNE(tsne_1, tsne_2, args.output_prefix, args.output_dir, df[args.test_field],"","gradient")
-    drawTSNE(tsne_1, tsne_2, args.output_prefix, args.output_dir, cluster_df['cluster'],"clustered","discrete")
+    if args.debug:
+        print(df.columns)
+    
+    for encoding in args.encodings:
+        tsne_1, tsne_2,accuracy = createTSNE(df, encoding)
+        cluster_df = cluster(tsne_1, tsne_2, alpha=5.0)
+        print(f"done with clustering for {encoding}, now drawing t-SNE plots")
+        drawTSNE(tsne_1, tsne_2, f"{args.output_prefix}_{encoding}", args.output_dir, df[args.test_field],"curated","gradient", range=args.range)
+        if(args.cluster):
+            drawTSNE(tsne_1, tsne_2, f"{args.output_prefix}_{encoding}_clustered", args.output_dir, cluster_df['cluster'],"clustered","discrete")
     #save the reverse mapping of cluster to arch index and arch string as a json
-    reverse_mapping = createReversemapping(df, cluster_df)
+    reverse_mapping = createReversemapping(df, cluster_df, args.code_field)
     with open(os.path.join(args.output_dir, f'{args.output_prefix}_reverse_mapping.txt'), 'w') as f:
         for cluster1, archs in reverse_mapping.items():
             f.write(f"Cluster {cluster1}:\n")
