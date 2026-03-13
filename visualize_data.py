e7e81149681b3e3a31d31aaee8841eca2a2eef32
Madhav <madhav.gullapalli9@gmail.com>
2026-03-12 21:03:52 -0400

    refactor vis_data

--- visualize_data.py
+++ visualize_data.py
@@ -22,7 +22,17 @@ def createTSNE(df, encoding):
 
     #return results
     return X_tsne[:,0], X_tsne[:,1], df['cifar10-valid_test_accuracy']
-def drawTSNE(tsne_1,tsne_2, FILE_NAME, OUTPUT_FOLDER, legend_name, plotting_number,name, plot_type, vrange = [0,1], pallete = 'viridis'):
+def drawTSNE(tsne_1,tsne_2, FILE_NAME, OUTPUT_FOLDER, legend_name, title_name, plotting_number, plot_type = "gradient", vrange = [0,1], pallete = 'viridis', reject = np.nan):
+    if title_name is None:
+        title_name = legend_name
+    #sort the tsne values by the plotting number so that the points with higher plotting number are plotted on top
+    sorted_indices = np.argsort(plotting_number)
+    #if legend_name is discovery_errorplot descending order, otherwise ascending order
+    if legend_name == "discovery_error":
+        sorted_indices = sorted_indices[::-1]
+    tsne_1 = tsne_1[sorted_indices]
+    tsne_2 = tsne_2[sorted_indices]
+    plotting_number = plotting_number[sorted_indices]
     OUTPUT_FILE = os.path.join(OUTPUT_FOLDER, FILE_NAME)
     plt.figure(figsize=(10, 8))
     #if plotting type is discreet : use a discret gradient or use continous gradient (deided by plotting number)
@@ -34,29 +44,21 @@ def drawTSNE(tsne_1,tsne_2, FILE_NAME, OUTPUT_FOLDER, legend_name, plotting_numb
     #delete data points that are outside the range
     if(plot_type == "gradient"):
         
-        mask_600 = (plotting_number == 600)
+        mask_reject = (plotting_number == reject)
        # Copy so we don't modify original
         plotting_number_clipped = plotting_number.copy()
 
         # Clip only non-600 values
-        plotting_number_clipped[~mask_600] = plotting_number_clipped[~mask_600].clip(
+        plotting_number_clipped[~mask_reject] = plotting_number_clipped[~mask_reject].clip(
             lower=vrange[0],
             upper=vrange[1]
         )# Plot all other points
         sc = plt.scatter(
-            tsne_1[~mask_600],
-            tsne_2[~mask_600],
-            c=plotting_number_clipped[~mask_600],
+            tsne_1[~mask_reject],
+            tsne_2[~mask_reject],
+            c=plotting_number_clipped[~mask_reject],
             cmap=pallete,
             alpha=0.3
-        )
-
-        plt.scatter(
-            tsne_1[mask_600],
-            tsne_2[mask_600],
-            color="darkred",
-            s=60,
-            edgecolor="black"
         )
 
         plt.colorbar(sc, label=legend_name)
@@ -65,13 +67,13 @@ def drawTSNE(tsne_1,tsne_2, FILE_NAME, OUTPUT_FOLDER, legend_name, plotting_numb
         sns.stripplot(x=tsne_1, y=tsne_2, hue=plotting_number, palette=paletteDiscrete[:plotting_number.nunique()], jitter=True)
         #create an underbar matching colors to clusters
         handles, labels = plt.gca().get_legend_handles_labels()
-        plt.legend(handles, labels, title=name, bbox_to_anchor=(1, 1), loc='upper left')
-    plt.title(f'tsne_plot_{name}_{FILE_NAME}')
+        plt.legend(handles, labels, title=legend_name, bbox_to_anchor=(1, 1), loc='upper left')
+    plt.title(f'tsne_plot_{title_name}_{FILE_NAME}')
     plt.xlabel('t-SNE Dimension 1')
     plt.ylabel('t-SNE Dimension 2')
     
-    plt.savefig(os.path.join(OUTPUT_FOLDER, f'tsne_plot_{name}_{FILE_NAME}.png'))
-    print(f"image saved at {os.path.join(OUTPUT_FOLDER, f'tsne_plot_{name}_{FILE_NAME}.png')}")
+    plt.savefig(os.path.join(OUTPUT_FOLDER, f'tsne_plot_{title_name}_{FILE_NAME}.png'))
+    print(f"image saved at {os.path.join(OUTPUT_FOLDER, f'tsne_plot_{title_name}_{FILE_NAME}.png')}")
     plt.show()
 
 def cluster(tsne_x, tsne_y, alpha):
@@ -94,16 +96,13 @@ def createReversemapping(df, cluster_df, code):
     return reverse_mapping    
 def main():
     parser = argparse.ArgumentParser(description='t-SNE Visualization of Genome Embeddings')
-    parser.add_argument('--input', type=str, default=f'/storage/ice-shared/vip-vvk/data/AOT/mgullapalli6/codenas/collecteddata_unified.csv', help='Path to the input CSV file containing the data')
-    parser.add_argument('--output_dir', type=str, default='/home/hice1/mgullapalli6/scratch/tsne', help='Directory to save the output visualization')
-    parser.add_argument('--output_prefix', type=str, default='onnx_main', help='Prefix for the output visualization file name')
+    parser.add_argument('--input', type=str, default=f'/storage/ice-shared/vip-vvk/data/AOT/mgullapalli6/codenas/collecteddata_unified80.csv', help='Path to the input CSV file containing the data')
+    parser.add_argument('--output_dir', type=str, default='/home/hice1/mgullapalli6/scratch/tsne/10001', help='Directory to save the output visualization')
     parser.add_argument('--encoding', type=str,  default='modernbert_large_true_onnx_encoding_fp16_embedding', help='Column name for the encoding to be visualized')
     
-    parser.add_argument('--test_fields', type=str, nargs='+', default=['cifar10-valid_test_accuracy', 'cifar100_valid_accuracy','predicted_accuracy_cifar10','predicted_accuracy_cifar100', 'generation_firstdiscovery_cifar10', 'generation_firstdiscovery_cifar100','average_predicted_accuracy_cifar10','average_predicted_accuracy_cifar100','average_generation_cifar10','average_generation_cifar100'], help='Column name for the field to be plotted as hue in the gradient plot')
     parser.add_argument('--code_field', type=str, default='pytorch_code_inline', help='Column name for the code field to be used in reverse mapping')
     parser.add_argument('--debug', type=bool, default=True, help='Whether to print debug information, such as the columns of the input dataframe')
     
-    parser.add_argument('--ranges', type=int, nargs='+', default=[80,100,50,100, 80, 100, 50, 100, 0, 700, 0, 700, 80, 100, 50, 100, 0, 700, 0, 700], help='Range of values to be plotted in the gradient plot')
     parser.add_argument('--cluster', type=bool, default=True, help='Whether to perform clustering on the t-SNE values and plot the clustered results')
     args = parser.parse_args()
     print("starting t-SNE visualization with the following arguments: ")
@@ -115,18 +114,34 @@ def main():
     tsne_1, tsne_2,accuracy = createTSNE(df, args.encoding)
     cluster_df = cluster(tsne_1, tsne_2, alpha=5.0)
     
-    for i, test_field in enumerate(args.test_fields):
-        drawTSNE(tsne_1, tsne_2, f"{args.output_prefix}_{test_field}", args.output_dir,test_field, df[test_field],"curated","gradient", vrange=[args.ranges[i*2], args.ranges[i*2+1]])
-    drawTSNE(tsne_1, tsne_2, f"{args.output_prefix}_cifar10_discovery_curbed_grad", args.output_dir,"generation", df['generation_firstdiscovery_cifar10'],"generation","gradient", vrange=[0, 100], pallete='crest')
-    drawTSNE(tsne_1, tsne_2, f"{args.output_prefix}_cifar100_discovery_curbed_grad", args.output_dir,"generation", df['generation_firstdiscovery_cifar100'],"generation","gradient", vrange=[0, 100], pallete='crest')
+
+    drawTSNE(tsne_1, tsne_2, f"tsne_average_generation_cifar10", args.output_dir,"generation", "average_generation_cifar10", df["average_generation_cifar10"],"gradient", vrange=[80,100], reject=-1000)
+    drawTSNE(tsne_1, tsne_2, f"tsne_average_generation_cifar100", args.output_dir,"generation", "average_generation_cifar100", df["average_generation_cifar100"],"gradient", vrange=[50,100], reject=-1000)
+    drawTSNE(tsne_1, tsne_2, f"tsne_average_predicted_accuracy_cifar10", args.output_dir,"predicted accuracy", "average_predicted_accuracy_cifar10", df["average_predicted_accuracy_cifar10"],"gradient", vrange=[80,100], reject=-1000)
+    drawTSNE(tsne_1, tsne_2, f"tsne_average_predicted_accuracy_cifar100", args.output_dir,"predicted accuracy", "average_predicted_accuracy_cifar100", df["average_predicted_accuracy_cifar100"],"gradient", vrange=[50,100], reject=-1000)
+    drawTSNE(tsne_1, tsne_2, f"tsne_average_real_accuracy_cifar10", args.output_dir,"accuracy", "average_real_accuracy_cifar10", df["cifar10-valid_test_accuracy"],"gradient", vrange=[0,20])
+    drawTSNE(tsne_1, tsne_2, f"tsne_average_real_accuracy_cifar100", args.output_dir,"accuracy", "average_real_accuracy_cifar100", df["cifar100_valid_accuracy"],"gradient", vrange=[0,20])
+    #draw errors in discovery with a continous plot (between correct and predicted accuracy) and cluster with a discrete plot, also if predicted acciracy = -1000, error should be -1000
+    df['discovery_error_cifar10'] = df.apply(lambda row: abs(row['cifar10-valid_test_accuracy'] - row['predicted_accuracy_cifar10']) if row['predicted_accuracy_cifar10'] != -1000 else -1000, axis=1)
+    df['discovery_error_cifar100'] = df.apply(lambda row: abs(row['cifar100_valid_accuracy'] - row['predicted_accuracy_cifar100']) if row['predicted_accuracy_cifar100'] != -1000 else -1000, axis=1)
+    drawTSNE(tsne_1, tsne_2, f"tsne_cifar10_discovery_error", args.output_dir,"discovery_error", "discovery_error_cifar10", df['discovery_error_cifar10'],"gradient", vrange=[0, 20], pallete = 'Reds', reject=-1000)
+    drawTSNE(tsne_1, tsne_2, f"tsne_cifar100_discovery_error", args.output_dir,"discovery_error", "discovery_error_cifar100", df['discovery_error_cifar100'],"gradient", vrange=[0, 20], pallete = 'Reds', reject=-1000)
+    #draw number of times showed up with a continous plot and cluster with a discrete plot, also if showed up = ０, then it should be colored differently (with a discrete color)
+    drawTSNE(tsne_1, tsne_2, f"tsne_cifar10_showed_up", args.output_dir,"showed_up", "showed_up_cifar10", df['showed_up_cifar10'],"gradient", vrange=[0, 20], pallete='crest',reject=-1000)
+    drawTSNE(tsne_1, tsne_2, f"tsne_cifar100_showed_up", args.output_dir,"showed_up", "showed_up_cifar100", df['showed_up_cifar100'],"gradient", vrange=[0, 20], pallete='crest', reject=-1000)
+    #draw real accuracy if preedicted accuracy is not -１０００ else set real accuracy to -１０００
+    df['real_accuracy_cifar10'] = df.apply(lambda row: row['cifar10-valid_test_accuracy'] if row['predicted_accuracy_cifar10'] != -1000 else -1000, axis=1)
+    df['real_accuracy_cifar100'] = df.apply(lambda row: row['cifar100_valid_accuracy'] if row['predicted_accuracy_cifar100'] != -1000 else -1000, axis=1)
+    drawTSNE(tsne_1, tsne_2, f"tsne_cifar10_real_accuracy", args.output_dir,"real_accuracy", "real_accuracy_cifar10", df['real_accuracy_cifar10'],"gradient", vrange=[80, 100], pallete='viridis', reject=-1000)
+    drawTSNE(tsne_1, tsne_2, f"tsne_cifar100_real_accuracy", args.output_dir,"real_accuracy", "real_accuracy_cifar100", df['real_accuracy_cifar100'],"gradient", vrange=[50, 100], pallete='viridis', reject=-1000)
     if(args.cluster):
-        drawTSNE(tsne_1, tsne_2, f"{args.output_prefix}_clustered", args.output_dir,"cluster", cluster_df['cluster'],"clustered","discrete")
+        drawTSNE(tsne_1, tsne_2, f"tsne_clustered", args.output_dir,"cluster", "clustered", cluster_df['cluster'],"discrete")
     #save the reverse mapping of cluster to arch index and arch string as a json
     reverse_mapping = createReversemapping(df, cluster_df, args.code_field)
-    with open(os.path.join(args.output_dir, f'{args.output_prefix}_reverse_mapping.txt'), 'w') as f:
+    with open(os.path.join(args.output_dir, f'reverse_mapping.txt'), 'w') as f:
         for cluster1, archs in reverse_mapping.items():
             f.write(f"Cluster {cluster1}:\n")
             for arch in archs:
                 f.write(f"  {arch}\n")
-    print(f"reverse mapping saved at {os.path.join(args.output_dir, f'{args.output_prefix}_reverse_mapping.txt')}")
+    print(f"reverse mapping saved at {os.path.join(args.output_dir, f'reverse_mapping.txt')}")
 main()
